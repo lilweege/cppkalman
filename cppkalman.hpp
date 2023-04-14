480e97be7d15d2b699beff753dfeed4fe1bda8b2
Luigi Quattrociocchi <luigi@quattrociocchi.net>
2023-04-14 14:51:43 -0400

    implement

--- cppkalman.hpp
+++ cppkalman.hpp
@@ -6,10 +6,28 @@
 #define CPPKALMAN_H
 
 #include <Eigen/Dense>
+#include <Eigen/Cholesky>
+
 #include <functional>
 #include <optional>
+#include <utility>
 
 namespace cppkalman {
+
+template<size_t N, typename T>
+struct Moments
+{
+    Eigen::Vector<T, N> stateMean;
+    Eigen::Matrix<T, N, N> stateCovariance;
+};
+
+template<size_t StateSize, size_t N, typename T>
+struct SigmaPoints
+{
+    Eigen::Matrix<T, 2*StateSize+1, N> points;
+    Eigen::Vector<T, 2*StateSize+1> weightsMean;
+    Eigen::Vector<T, 2*StateSize+1> weightsCovariance;
+};
 
 template<size_t StateSize, size_t ObservationSize, typename T=double>
 class AdditiveUnscentedKalmanFilter
@@ -22,22 +40,17 @@ public:
     using TransitionFunction = std::function<State(State)>;
     using ObservationFunction = std::function<Observation(State)>;
 
-    struct Sample {
-        State filteredStateMean;
-        StateCovariance filteredStateCovariance;
-    };
-
     AdditiveUnscentedKalmanFilter(TransitionFunction&&, ObservationFunction&&);
 
-    void Predict(Sample);
-    Sample Update(std::optional<Observation>);
+    void Predict(const Moments<StateSize, T>&);
+    Moments<StateSize, T> Update(const std::optional<Observation>&);
 
-private:
-    TransitionFunction mTransitionFunction;
-    ObservationFunction mObservationFunction;
+public:
+    const TransitionFunction mTransitionFunction;
+    const ObservationFunction mObservationFunction;
 
-    T mMomentsPredicted;
-    T mPointsPredicted;
+    Moments<StateSize, T> mMomentsPredicted;
+    SigmaPoints<StateSize, StateSize, T> mPointsPredicted;
 };
 
 
@@ -50,26 +63,135 @@ private:
 namespace cppkalman {
 
 
+template<size_t StateSize, typename T>
+SigmaPoints<StateSize, StateSize, T>
+static
+Moments2Points(const Moments<StateSize, T>& moments)
+{
+    SigmaPoints<StateSize, StateSize, T> points;
+    const auto& [mu, sigma] = moments;
+    const T alpha = 1.0;
+    const T beta = 0.0;
+    const T kappa = 3.0 - StateSize;
+
+    Eigen::LLT<Eigen::Matrix<T, StateSize, StateSize>, Eigen::Upper> llt{sigma};
+    Eigen::Matrix<T, StateSize, StateSize> sigma2 = llt.matrixLLT().transpose();
+    // Clear upper triangle
+    for (size_t i = 0; i < StateSize; ++i)
+        for (size_t j = i+1; j < StateSize; ++j)
+            sigma2(i, j) = 0;
+
+    const T lambda = (alpha * alpha) * (StateSize + kappa) - StateSize;
+    const T c = StateSize + lambda;
+
+    for (size_t i = 0; i < StateSize; ++i) {
+        for (size_t j = 0; j < 2*StateSize+1; ++j)
+            points.points(j, i) = mu(i);
+        for (size_t j = 1; j < StateSize + 1; ++j)
+            points.points(j, i) += sigma2(i, j-1) * sqrt(c);
+        for (size_t j = StateSize + 1; j < 2*StateSize+1; ++j)
+            points.points(j, i) -= sigma2(i, j-(StateSize+1)) * sqrt(c);
+    }
+
+    points.weightsMean.setOnes();
+    points.weightsMean(0) = lambda / c;
+    for (size_t i = 1; i < 2*StateSize+1; ++i)
+        points.weightsMean(i) = 0.5 / c;
+
+    points.weightsCovariance = points.weightsMean;
+    points.weightsCovariance(0) = lambda / c + (1 - alpha * alpha + beta);
+
+    return points;
+}
+
+
+template<size_t StateSize, size_t ObservationSize, typename T>
+Moments<ObservationSize, T>
+static
+Points2Moments(const SigmaPoints<StateSize, ObservationSize, T>& sigmaPoints)
+{
+    Moments<ObservationSize, T> moments;
+    const auto& [points, weightsMean, weightsCovariance] = sigmaPoints;
+    
+    moments.stateMean = points.transpose() * weightsMean;
+    Eigen::Matrix<T, ObservationSize, 2*StateSize+1> pointsDiff = points.transpose();
+    for (size_t i = 0; i < ObservationSize; ++i)
+        for (size_t j = 0; j < 2*StateSize+1; ++j)
+            pointsDiff(i, j) -= moments.stateMean(i);
+
+
+    moments.stateCovariance.setIdentity();
+    moments.stateCovariance += pointsDiff * weightsCovariance.asDiagonal() * pointsDiff.transpose();
+
+    return moments;
+}
+
+
+template<size_t StateSize, size_t ObservationSize, typename T>
+std::pair<SigmaPoints<StateSize, ObservationSize, T>, Moments<ObservationSize, T>>
+static
+UnscentedTransform(const SigmaPoints<StateSize, StateSize, T>& sigmaPoints, auto f) // FIXME: constrain function with concept
+{
+    SigmaPoints<StateSize, ObservationSize, T> points;
+    points.weightsCovariance = sigmaPoints.weightsCovariance;
+    points.weightsMean = sigmaPoints.weightsMean;
+    for (size_t i = 0; i < 2*StateSize+1; ++i) {
+        Eigen::Vector<T, ObservationSize> row = f(sigmaPoints.points.row(i));
+        for (size_t j = 0; j < row.size(); ++j)
+            points.points(i, j) = row(j);
+    }
+
+    return std::make_pair(points, Points2Moments(points));
+}
+
+
 template<size_t StateSize, size_t ObservationSize, typename T>
 AdditiveUnscentedKalmanFilter<StateSize, ObservationSize, T>::AdditiveUnscentedKalmanFilter(TransitionFunction&& transitionFunction, ObservationFunction&& observationFunction)
     : mTransitionFunction{transitionFunction}
     , mObservationFunction{observationFunction}
 {
-    
 }
+
 
 template<size_t StateSize, size_t ObservationSize, typename T>
 void
-AdditiveUnscentedKalmanFilter<StateSize, ObservationSize, T>::Predict(Sample currentSample)
+AdditiveUnscentedKalmanFilter<StateSize, ObservationSize, T>::Predict(const Moments<StateSize, T>& momentsState)
 {
+    SigmaPoints pointsState = Moments2Points(momentsState);
+    auto [_, moments] = UnscentedTransform<StateSize, StateSize, T>(pointsState, mTransitionFunction);
 
+    mMomentsPredicted = std::move(moments);
+    mPointsPredicted = Moments2Points(mMomentsPredicted);
 }
 
 template<size_t StateSize, size_t ObservationSize, typename T>
-AdditiveUnscentedKalmanFilter<StateSize, ObservationSize, T>::Sample
-AdditiveUnscentedKalmanFilter<StateSize, ObservationSize, T>::Update(std::optional<Observation> observation)
+Moments<StateSize, T>
+AdditiveUnscentedKalmanFilter<StateSize, ObservationSize, T>::Update(const std::optional<Observation>& observation)
 {
-    return {};
+    if (!observation)
+        return mMomentsPredicted;
+
+    Moments<StateSize, T> momentsFiltered;
+    auto [obsPointsPredicted, obsMomentsPredicted] = UnscentedTransform<StateSize, ObservationSize, T>(mPointsPredicted, mObservationFunction);
+
+    auto x = mPointsPredicted.points;
+    for (size_t i = 0; i < 2*StateSize+1; ++i)
+        for (size_t j = 0; j < StateSize; ++j)
+            x(i, j) -= mMomentsPredicted.stateMean(j);
+    auto y = obsPointsPredicted.points;
+    for (size_t i = 0; i < 2*StateSize+1; ++i)
+        for (size_t j = 0; j < StateSize; ++j)
+            y(i, j) -= obsMomentsPredicted.stateMean(j);
+    
+    auto crossSigma = x.transpose() * mPointsPredicted.weightsMean.asDiagonal() * y;
+
+    // Kalman gain
+    auto K = crossSigma * obsMomentsPredicted.stateCovariance.completeOrthogonalDecomposition().pseudoInverse();
+    // Correct
+    momentsFiltered.stateMean = mMomentsPredicted.stateMean + K * (*observation - obsMomentsPredicted.stateMean);
+    momentsFiltered.stateCovariance = mMomentsPredicted.stateCovariance - K * crossSigma.transpose();
+    
+    return momentsFiltered;
 }
 
 
--- example.cpp
+++ example.cpp
@@ -6,30 +6,30 @@ using namespace cppkalman;
 
 constexpr size_t STATE_SIZE = 6;
 constexpr size_t OBSERVATION_SIZE = 3;
-using KalmanFilter = AdditiveUnscentedKalmanFilter<STATE_SIZE, OBSERVATION_SIZE>;
-
+using T = double;
+using KalmanFilter = AdditiveUnscentedKalmanFilter<STATE_SIZE, OBSERVATION_SIZE, T>;
 
 // Unfortunately Eigen does not have constexpr constructors yet
 KalmanFilter::State Transition(KalmanFilter::State state)
 {
-    constexpr double dt = 1.0;
-    static const Eigen::Matrix<double, STATE_SIZE, STATE_SIZE> F{{
-        1.0, 0.0, 0.0,  dt, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0,  dt, 0.0,
-        0.0, 0.0, 1.0, 0.0, 0.0,  dt,
-        0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
-        0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
-        0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+    constexpr T dt = 1.0;
+    static const Eigen::Matrix<T, STATE_SIZE, STATE_SIZE> F{{
+        { 1.0, 0.0, 0.0,  dt, 0.0, 0.0 },
+        { 0.0, 1.0, 0.0, 0.0,  dt, 0.0 },
+        { 0.0, 0.0, 1.0, 0.0, 0.0,  dt },
+        { 0.0, 0.0, 0.0, 1.0, 0.0, 0.0 },
+        { 0.0, 0.0, 0.0, 0.0, 1.0, 0.0 },
+        { 0.0, 0.0, 0.0, 0.0, 0.0, 1.0 },
     }};
     return F * state;
 }
 
 KalmanFilter::Observation Observation(KalmanFilter::State state)
 {
-    static const Eigen::Matrix<double, OBSERVATION_SIZE, STATE_SIZE> H{{
-        1.0, 0.0, 0.0, 0,0, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0, 0,0, 0.0,
-        0.0, 0.0, 1.0, 0.0, 0.0, 0,0,
+    static const Eigen::Matrix<T, OBSERVATION_SIZE, STATE_SIZE> H{{
+        { 1.0, 0.0, 0.0, 0,0, 0.0, 0.0 },
+        { 0.0, 1.0, 0.0, 0.0, 0,0, 0.0 },
+        { 0.0, 0.0, 1.0, 0.0, 0.0, 0,0 },
     }};
     return H * state;
 }
@@ -39,7 +39,19 @@ int main()
 {
     KalmanFilter kf{Transition, Observation};
 
-    KalmanFilter::Sample s;
-    kf.Predict(s);
-    s = kf.Update(std::nullopt);
+    Moments<STATE_SIZE, T> current;
+    current.stateMean << 8.892125, -3.183644, -0.766539, 0., 0., 0.;
+    current.stateCovariance << 
+        100,   0,     0,     0,     0,     0,
+        0, 10000,     0,     0,     0,     0,
+        0,     0,   100,     0,     0,     0,
+        0,     0,     0, 10000,     0,     0,
+        0,     0,     0,     0,   100,     0,
+        0,     0,     0,     0,     0, 10000;
+        
+    kf.Predict(current);
+
+    KalmanFilter::Observation obs{{ 8.892125, -3.183644, -0.766539 }};
+    Moments<STATE_SIZE, T> next = kf.Update(obs);
+    std::cout << "OK!\n";
 }
