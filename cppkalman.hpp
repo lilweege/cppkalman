9d47e1758f6a7a64503afe462e98f657aafb0500
Luigi Quattrociocchi <luigi@quattrociocchi.net>
2023-04-20 03:53:44 -0400

    documentation

--- cppkalman.hpp
+++ cppkalman.hpp
@@ -1,4 +1,7 @@
-/*
+/* cppkalman - v1.0 - A port of pykalman's AdditiveUnscentedKalmanFilter to C++
+This library is header-only (see https://github.com/nothings/stb#how-do-i-use-these-libraries)
+
+Luigi Quattrociocchi - April 20, 2023
 */
 
 
@@ -29,6 +32,13 @@ struct SigmaPoints
     Eigen::Vector<T, 2*StateSize+1> weightsCovariance;
 };
 
+/**
+ * A port of pykalman's AdditiveUnscentedKalmanFilter.
+ *
+ * @tparam StateSize The number of elements in the state vector.
+ * @tparam ObservationSize The number of elements in the observation vector.
+ * @tparam T The desired floating point type, defaults to double.
+ */
 template<int StateSize, int ObservationSize, typename T=double>
 class AdditiveUnscentedKalmanFilter
 {
@@ -40,9 +50,33 @@ public:
     using TransitionFunction = std::function<State(State)>;
     using ObservationFunction = std::function<Observation(State)>;
 
+    /**
+     * @param[in] transitionFunction A function describing how the state changes between times t and t+1.
+     * @param[in] observationFunction A function characterizing how the observation at time t+1 is generated.
+     */
     AdditiveUnscentedKalmanFilter(TransitionFunction&&, ObservationFunction&&);
 
+    /**
+     * Predict next state distribution
+     *
+     * Using the sigma points representing the state at time t given observations
+     * from time steps 0...t, calculate the predicted mean, covariance, and sigma
+     * points for the state at time t+1.
+     * 
+     * @param[in] currentState the state at time step t given observations from time steps 0...t.
+     * @returns A pair containings: (1) predicted mean state and covariance corresponding to time step t+1,
+     * and (2) sigma points corresponding to the predicted state.
+     */
     [[nodiscard]] std::pair<Moments<StateSize, T>, SigmaPoints<StateSize, StateSize, T>> Predict(const Moments<StateSize, T>&);
+
+    /**
+     * Integrate new observation to correct state estimates
+     * 
+     * @param[in] predictedState Predicted mean state and covariance corresponding to time step t+1.
+     * @param[in] predictedPoints Sigma points corresponding to predictedState.
+     * @param[in] observation An (optional) observation at time t+1. If no observation is provided, predictedState is returned.
+     * @returns Filtered mean state and covariance corresponding to time step t+1.
+    */
     [[nodiscard]] Moments<StateSize, T> Update(const Moments<StateSize, T>&, const SigmaPoints<StateSize, StateSize, T>&, const std::optional<Observation>&);
 
 public:
@@ -59,7 +93,12 @@ public:
 #ifdef CPPKALMAN_IMPL
 namespace cppkalman {
 
-
+/**
+ * Calculate "sigma points" used in Unscented Kalman Filter.
+ * 
+ * @param[in] moments Mean and covariance of a multivariate normal.
+ * @returns Sigma points and associated weights.
+*/
 template<int StateSize, typename T>
 SigmaPoints<StateSize, StateSize, T>
 static
@@ -101,6 +140,12 @@ Moments2Points(const Moments<StateSize, T>& moments)
 }
 
 
+/**
+ * Calculate estimated mean and covariance of sigma points.
+ * 
+ * @param[in] sigmaPoints SigmaPoints object containing points and weights.
+ * @returns Mean and covariance estimated using points.
+*/
 template<int StateSize, int ObservationSize, typename T>
 Moments<ObservationSize, T>
 static
@@ -122,6 +167,16 @@ Points2Moments(const SigmaPoints<StateSize, ObservationSize, T>& sigmaPoints)
     return moments;
 }
 
+/**
+ * Apply the Unscented Transform to a set of points.
+ * 
+ * Apply f to points (with secondary argument points_noise, if available),
+ * then approximate the resulting mean and covariance. If sigma_noise is
+ * available, treat it as additional variance due to additive noise.
+ * 
+ * @param[in] sigmaPoints Points to pass into f's first argument and associated weights.
+ * @param[in] f transition function from time t to time t+1.
+*/
 template<int StateSize, int ObservationSize, typename T, typename Function>
 std::pair<Moments<ObservationSize, T>, SigmaPoints<StateSize, ObservationSize, T>>
 static
@@ -153,9 +208,9 @@ AdditiveUnscentedKalmanFilter<StateSize, ObservationSize, T>::AdditiveUnscentedK
 template<int StateSize, int ObservationSize, typename T>
 std::pair<Moments<StateSize, T>, SigmaPoints<StateSize, StateSize, T>>
 AdditiveUnscentedKalmanFilter<StateSize, ObservationSize, T>::Predict(
-    const Moments<StateSize, T>& momentsState)
+    const Moments<StateSize, T>& currentState)
 {
-    SigmaPoints pointsState = Moments2Points(momentsState);
+    SigmaPoints pointsState = Moments2Points(currentState);
     auto [moments, _] = UnscentedTransform<StateSize, StateSize, T>(pointsState, mTransitionFunction);
     return std::make_pair(moments, Moments2Points(moments));
 }
@@ -163,33 +218,33 @@ AdditiveUnscentedKalmanFilter<StateSize, ObservationSize, T>::Predict(
 template<int StateSize, int ObservationSize, typename T>
 Moments<StateSize, T>
 AdditiveUnscentedKalmanFilter<StateSize, ObservationSize, T>::Update(
-    const Moments<StateSize, T>& momentsPredicted,
-    const SigmaPoints<StateSize, StateSize, T>& pointsPredicted,
+    const Moments<StateSize, T>& predictedState,
+    const SigmaPoints<StateSize, StateSize, T>& predictedPoints,
     const std::optional<Observation>& observation)
 {
     if (!observation)
-        return momentsPredicted;
+        return predictedState;
 
-    auto [obsMomentsPredicted, obsPointsPredicted] = UnscentedTransform<StateSize, ObservationSize, T>(pointsPredicted, mObservationFunction);
+    auto [obsMomentsPredicted, obsPointsPredicted] = UnscentedTransform<StateSize, ObservationSize, T>(predictedPoints, mObservationFunction);
 
-    auto predDiff = pointsPredicted.points;
+    auto predDiff = predictedPoints.points;
     for (int i = 0; i < 2*StateSize+1; ++i)
         for (int j = 0; j < StateSize; ++j)
-            predDiff(i, j) -= momentsPredicted.stateMean(j);
+            predDiff(i, j) -= predictedState.stateMean(j);
     auto obsDiff = obsPointsPredicted.points;
     for (int i = 0; i < 2*StateSize+1; ++i)
         for (int j = 0; j < ObservationSize; ++j)
             obsDiff(i, j) -= obsMomentsPredicted.stateMean(j);
     
-    auto crossSigma = predDiff.transpose() * pointsPredicted.weightsMean.asDiagonal() * obsDiff;
+    auto crossSigma = predDiff.transpose() * predictedPoints.weightsMean.asDiagonal() * obsDiff;
 
     // Kalman gain
     auto K = crossSigma * obsMomentsPredicted.stateCovariance.completeOrthogonalDecomposition().pseudoInverse();
 
     // Correct
     Moments<StateSize, T> momentsFiltered;
-    momentsFiltered.stateMean = momentsPredicted.stateMean + K * (*observation - obsMomentsPredicted.stateMean);
-    momentsFiltered.stateCovariance = momentsPredicted.stateCovariance - K * crossSigma.transpose();
+    momentsFiltered.stateMean = predictedState.stateMean + K * (*observation - obsMomentsPredicted.stateMean);
+    momentsFiltered.stateCovariance = predictedState.stateCovariance - K * crossSigma.transpose();
     
     return momentsFiltered;
 }
