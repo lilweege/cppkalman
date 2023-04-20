437fc173cdc4d25d62732fe5cb37f5a95190c760
Luigi Quattrociocchi <luigi@quattrociocchi.net>
2023-04-20 02:50:52 -0400

    minimal example

---
+++ minimal.cpp
@@ -0,0 +1,39 @@
+// Define macro to enable function definitions for the header only library
+// To learn more, see https://github.com/nothings/stb#how-do-i-use-these-libraries
+#define CPPKALMAN_IMPL
+#include "cppkalman.hpp"
+
+#include <iostream>
+
+int main()
+{
+    // A one-dimensional constant velocity model
+    // State vector:       [xp, xv]
+    // Observation vector: [xp]
+    // The template parameters <2, 1> are the sizes of these vectors, respectively
+    using KF = cppkalman::AdditiveUnscentedKalmanFilter<2, 1>;
+
+    // Define (potentially non-linear) transition and observation functions
+    auto transitionFunc = [](KF::State state) -> KF::State {
+        state(0) += state(1); // xp += xv
+        return state;
+    };
+    auto observationFunc = [](KF::State state) -> KF::Observation {
+        return state.head(1); // xp
+    };
+    KF kf{transitionFunc, observationFunc};
+
+    // Predict
+    cppkalman::Moments<2> currentState;
+    currentState.stateMean << 2.2, 1.1; // Predicted position, velocity
+    currentState.stateCovariance << 1.0, 0.0, 0.0, 1.0;
+    auto [predictedState, sigmaPoints] = kf.Predict(currentState);
+
+    // Update
+    decltype(kf)::Observation currentObservation;
+    currentObservation << 3.5; // Observed position
+    auto nextState = kf.Update(predictedState, sigmaPoints, currentObservation);
+
+    std::cout << observationFunc(nextState.stateMean) << '\n'; // Filtered position
+    // The filtered position should be between predicted (3.3) and obsereved (3.5)
+}
